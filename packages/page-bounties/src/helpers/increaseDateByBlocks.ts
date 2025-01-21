849696bfe4f519ceb9381175fdea9ccc6652af98
ultrashiny <111131637+ultrashiny@users.noreply.github.com>
2025-01-22 00:19:45 +0900

    refactor: remove unnecessary pages(test)

--- packages/page-bounties/src/helpers/increaseDateByBlocks.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2017-2024 @polkadot/app-treasury authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-import type { BN } from '@polkadot/util';
-
-export function increaseDateByBlocks (blocks: BN, blockTime: number): Date {
-  return new Date(Date.now() + blocks.muln(blockTime).toNumber());
-}
