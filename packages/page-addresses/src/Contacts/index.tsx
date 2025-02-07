80c82ff931cd39a2f6100b863af66b9f62cf71c7
fromancient <cantillo.joiner@technoincor.com>
2025-02-06 21:11:26 -0500

    Update index.tsx
    Co-Authored-By: 10-ET | TCL <196585532+10-ET@users.noreply.github.com>
    Co-Authored-By: CDL <94694049+crazydevlegend@users.noreply.github.com>

--- packages/page-addresses/src/Contacts/index.tsx
+++ packages/page-addresses/src/Contacts/index.tsx
@@ -107,7 +107,7 @@ const StyledDiv = styled.div`
     border-radius: 1rem;
     padding: 0 3rem;
     overflow: auto;
-    height: calc(100% - 80px);
+    height: calc(100% - 80px); // height 
     table {
       margin-bottom: 0 !important;
       border-radius: 5px;
